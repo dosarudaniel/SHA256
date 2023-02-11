a3bd951e58d1bca8c565c675e3b33f9ce76dbe29
danidosa <daniel.dosaru@keysight.com>
2023-02-11 19:25:27 +0200

    Added test:  Measure the execution time of hashing a message depending on its length

--- sha256.c
+++ sha256.c
@@ -280,43 +280,76 @@ void sha256(const void *data, size_t len, uint8_t *hash)
 #ifdef SHA256_SELF_TEST__
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PACKET_SIZE_BYTES 1024
+#define HEADERS_SIZE_BYTES 64
+
+#define NR_ITERATIONS 10000
+#define NR_ELEMENTS 10
 
 int main(void)
 {
-    char *buf[] = {
-        "",
-        "e3b0c442 98fc1c14 9afbf4c8 996fb924 27ae41e4 649b934c a495991b 7852b855",
+    uint8_t packets[NR_ELEMENTS][PACKET_SIZE_BYTES];
+    uint8_t layer_3_headers[NR_ELEMENTS][HEADERS_SIZE_BYTES];
 
-        "abc",
-        "ba7816bf 8f01cfea 414140de 5dae2223 b00361a3 96177a9c b410ff61 f20015ad",
-
-        "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
-        "248d6a61 d20638b8 e5c02693 0c3e6039 a33ce459 64ff2167 f6ecedd4 19db06c1",
-
-        "The quick brown fox jumps over the lazy dog",
-        "d7a8fbb3 07d78094 69ca9abc b0082e4f 8d5651e4 6d3cdb76 2d02d0bf 37c9e592",
-
-        "The quick brown fox jumps over the lazy cog", // avalanche effect test
-        "e4c4d8f3 bf76b692 de791a17 3e053211 50f7a345 b46484fe 427f6acc 7ecc81be",
-
-        "bhn5bjmoniertqea40wro2upyflkydsibsk8ylkmgbvwi420t44cq034eou1szc1k0mk46oeb7ktzmlxqkbte2sy",
-        "9085df2f 02e0cc45 5928d0f5 1b27b4bf 1d9cd260 a66ed1fd a11b0a3f f5756d99"
-    };
-    const size_t tests_total = sizeof(buf) / sizeof(buf[0]);
+    clock_t start, end;
+    double cpu_time_used;
     uint8_t hash[SHA256_SIZE_BYTES];
 
-    if (0 != (tests_total % 2)) {
-        return printf("invalid tests\n");
+    // Populate buffers with random numbers
+
+    srand(time(0));
+    printf("Populate random values in the packet of size %d \n", PACKET_SIZE_BYTES);
+    for (size_t j = 0; j < NR_ELEMENTS; j++)
+    {
+        for (size_t i = 0; i < PACKET_SIZE_BYTES; i++)
+        {
+            packets[j][i] = (char) rand();
+            //printf("%x", packets[j][i]);
+        }
+       // printf("\n");
     }
 
-    for (size_t i = 0; i < tests_total; i += 2) {
-        sha256(buf[i], strlen(buf[i]), hash);
-        printf("input = '%s'\ndigest: %s\nresult: ", buf[i], buf[i + 1]);
-        for (size_t j = 0; j < SHA256_SIZE_BYTES; j++) {
-            printf("%02x%s", hash[j], ((j % 4) == 3) ? " " : "");
+    printf("Populate random values in the layer_3_headers of size %d \n", HEADERS_SIZE_BYTES);
+    for (size_t j = 0; j < NR_ELEMENTS; j++)
+    {
+        for (size_t i = 0; i < HEADERS_SIZE_BYTES; i++)
+        {
+            layer_3_headers[j][i] = (char) rand();
+            //printf("%x", layer_3_headers[j][i]);
         }
-        printf("\n\n");
+        //printf("\n");
     }
+
+    // Hash packets
+    start = clock();
+    for (size_t i = 0; i < NR_ITERATIONS; i++) {
+        for (size_t j = 0; j < NR_ELEMENTS; j++)
+        {
+            sha256(packets[j], PACKET_SIZE_BYTES, hash);
+        }
+    }
+    end = clock();
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("%d packets of size %d hashing took %f seconds to execute \n", NR_ELEMENTS*NR_ITERATIONS, PACKET_SIZE_BYTES, cpu_time_used);
+
+
+    // Hash layer_3 headers
+    start = clock();
+    for (size_t i = 0; i < NR_ITERATIONS; i++) {
+        for (size_t j = 0; j < NR_ELEMENTS; j++)
+        {
+            sha256(layer_3_headers[j], HEADERS_SIZE_BYTES, hash);
+        }
+    }
+    end = clock();
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("%d layer 3 headers of size %d hashing took %f seconds to execute \n", NR_ELEMENTS*NR_ITERATIONS, HEADERS_SIZE_BYTES, cpu_time_used);
 
     return 0;
 } // main
